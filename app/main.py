af82ebdfc4377bed7911fb1e089c4285dc361fc2
paula <paula.pamplona.martins@gmail.com>
2022-09-01 11:45:52 +0100

    Added query method and find by ID method

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import FastAPI, Body, Response, status, HTTPException, Depends
+from fastapi import FastAPI, Body, Response, status, HTTPException, Depends, Query
 from models import PostBase, PostCreate, PostOut
 from typing import Optional, List
 
@@ -18,14 +18,31 @@ def root():
 
 @app.get("/posts", status_code=status.HTTP_200_OK, response_model=List[PostOut])
 def get_posts():
-    return posts
+        return posts
+
+@app.get("/posts/", status_code=status.HTTP_200_OK, response_model=PostOut)
+def get_posts(post_id: Optional[int] = None):
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+    if post_id not in posts:
+        raise HTTPException(status_code=404)
+
+
+@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK, response_model=PostOut)
+def get_posts_id(post_id: int):
+    for post in posts:
+        if post['id'] == post_id:
+            return post
+    if post_id not in posts:
+        raise HTTPException(status_code=404)
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=PostOut)
 def create_posts(post: PostCreate):
-    post_id = posts[-1]['id'] + 1 # Creating a new ID that increases from the previous ID value in posts
+    post_id = posts[-1]['id'] + 1  # Creating a new ID that increases from the previous ID value in posts
     new_post = dict(post)
     new_post.update(id=post_id, created_at=datetime.now())
     print(new_post)
-    posts.append(new_post) # Puts new post in posts list
+    posts.append(new_post)  # Puts new post in posts list
     return new_post
